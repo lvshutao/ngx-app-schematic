ff00e759c4bdcb3495fa617e03be4f1e1b7c1b1d
lvshutao <525191782@qq.com>
2022-03-24 20:50:43 +0800

    基本的框架

--- apps/website/jest.config.js
+++ apps/website/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nglibx-schematic',
+  displayName: 'website',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/nglibx-schematic',
+  coverageDirectory: '../../coverage/apps/website',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
